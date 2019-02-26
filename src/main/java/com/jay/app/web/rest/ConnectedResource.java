e163b0c80d35fdba10e20d2eae31e658349bf9e2
Dhananjay Raparla <dhananjay.raparla@gmail.com>
2019-02-25 20:39:01 -0500

    Added city.txt with test values and verified its available via api.

--- src/main/java/com/jay/app/web/rest/ConnectedResource.java
+++ src/main/java/com/jay/app/web/rest/ConnectedResource.java
@@ -2,6 +2,8 @@ package com.jay.app.web.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/connected")
 public class ConnectedResource {
 
+    @Value("classpath:city.txt")
+    Resource resourceFile;
+
     private final Logger log = LoggerFactory.getLogger(ConnectedResource.class);
 
     /**
      * GET connected
      */
     @GetMapping("/connected")
-    public String connected() {
-        return "connected";
+    public Resource connected() {
+        return resourceFile;
     }
 
 }
